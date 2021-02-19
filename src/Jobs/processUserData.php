88cd7933c60e61e05237c968f8c79a654eb65435
Aidan Laycock <aidanl_@hotmail.co.uk>
2021-02-19 10:39:25 +0000

    Refactor to processUserData job.

--- src/Jobs/processUserData.php
+++ src/Jobs/processUserData.php
@@ -4,10 +4,12 @@ namespace BristolSU\UnionCloud\Jobs;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use BristolSU\Support\User\User;
+use Illuminate\Support\Facades\Log;
 
 class ProcessUserData implements ShouldQueue
 {
@@ -35,13 +37,10 @@ class ProcessUserData implements ShouldQueue
     {
         $this->repository = app(\BristolSU\ControlDB\Contracts\Repositories\User::class);
 
-        $User = $this->repository->getById($this->user['uid']);
-
-        if($User) {
-            // If Exists Update:
-            $this->repository->update($this->user['uid'], $User->id());
-        } else {
-            // If doesn't exist then insert:
+        try {
+            $User = $this->repository->getByDataProviderId($this->user['uid']);
+            $this->repository->update($User->id(), $this->user['uid']);
+        } catch (ModelNotFoundException $e) {
             $this->repository->create($this->user['uid']);
         }
     }
